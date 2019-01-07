3f03d0d54cc2fce5d498b35707b470f69a606a8f
petek157 <petek157@peter-korugas-macbook-pro-1625.local>
2019-01-06 22:05:44 -0600

    Push to remote

---
+++ WashShop/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  WashShop
+//
+//  Created by Peter Koruga on 1/6/19.
+//  Copyright © 2019 Peter Koruga. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "Particle-SDK.h"
+
+#endif /* Bridging_Header_h */
--- WashShop/ViewController.swift
+++ WashShop/ViewController.swift
@@ -10,11 +10,112 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var washDoorButton: UIButton!
+    @IBOutlet weak var mechDoorButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var deviceNameLabel: UILabel!
+    @IBOutlet weak var deviceIdLabel: UILabel!
+    
+    var shopDevice: ParticleDevice?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        if ParticleCloud.sharedInstance().isAuthenticated {
+            loginButton.isUserInteractionEnabled = false
+            logoutButton.isUserInteractionEnabled = true
+            washDoorButton.isUserInteractionEnabled = true
+            mechDoorButton.isUserInteractionEnabled = true
+        } else {
+            loginButton.isUserInteractionEnabled = true
+            logoutButton.isUserInteractionEnabled = false
+            washDoorButton.isUserInteractionEnabled = false
+            mechDoorButton.isUserInteractionEnabled = false
+        }
+
     }
 
-
+    func getWashShop() {
+        ParticleCloud.sharedInstance().getDevices { (devices:[ParticleDevice]?, error:Error?) -> Void in
+            if let _ = error {
+                print("Check your internet connectivity")
+            }
+            else {
+                if let d = devices {
+                    for device in d {
+                        if device.name == "garage_doors" {
+                            self.shopDevice = device
+                            
+                            if let name = self.shopDevice?.name {
+                                self.deviceNameLabel.text = name
+                            }
+                            
+                            if let id = self.shopDevice?.id {
+                                self.deviceIdLabel.text = id
+                            }
+                            
+                            self.washDoorButton.isUserInteractionEnabled = true
+                            self.mechDoorButton.isUserInteractionEnabled = true
+                            break
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func washDoorButtonPressed(_ sender: Any) {
+        
+        //let funcArgs = ["D7",1]
+        var task = shopDevice!.callFunction("tleftdoor", withArguments: nil) { (resultCode : NSNumber?, error : Error?) -> Void in
+            if (error == nil) {
+                print("Wash Door Toggled")
+            } else {
+                print("Wash Door Error: \(error)")
+            }
+        }
+        var bytesToReceive : Int64 = task.countOfBytesExpectedToReceive
+        // ..do something with bytesToReceive
+        
+    }
+    
+    @IBAction func mechDoorButtonPressed(_ sender: Any) {
+        
+        var task = shopDevice!.callFunction("trightdoor", withArguments: nil) { (resultCode : NSNumber?, error : Error?) -> Void in
+            if (error == nil) {
+                print("Mech Door Toggled")
+            } else {
+                print("Mech Door Error: \(error)")
+            }
+        }
+        var bytesToReceive : Int64 = task.countOfBytesExpectedToReceive
+        // ..do something with bytesToReceive
+    
+    }
+    
+    func loginUser() {
+        ParticleCloud.sharedInstance().login(withUser: "petek157@hotmail.com", password: "PK79CNKB") { (error) in
+            if let e = error {
+                print("Particle Error: \(e)")
+            } else {
+                print("Logged In")
+                
+                self.getWashShop()
+                
+            }
+        }
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        loginUser()
+    }
+    
+    @IBAction func logoutPressed(_ sender: Any) {
+        ParticleCloud.sharedInstance().logout()
+        print("Logged Out")
+        washDoorButton.isUserInteractionEnabled = false
+        mechDoorButton.isUserInteractionEnabled = false
+    }
 }
 
